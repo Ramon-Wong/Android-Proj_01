143af3e35aefe24ae44d7348471ce1a3b4dfeebf
Ramon-Wong <rcnwong78@gmail.com>
2014-06-21 09:43:00 -0300

    First Commit

---
+++ src/com/exam/game/run/GameRun.java
@@ -0,0 +1,15 @@
+package com.exam.game.run;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameRun extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
