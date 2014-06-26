9883a352aa5a8040332eebbdba6a7319929cd12e
Ramon-Wong <rcnwong78@gmail.com>
2014-06-25 22:32:16 -0300

    It work, and finally fix the strings.xml bug (order of opengl start up in the activity thread)

--- src/com/exam/game/run/GL.java
+++ src/com/exam/game/run/GL.java
@@ -1,149 +1,40 @@
 package com.exam.game.run;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLES20;
-
-import android.opengl.GLU;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.os.SystemClock;
 
 
 
+public class GL implements GLSurfaceView.Renderer
+{
+	public GL(){	
 
-
-
-public class GL implements	Renderer {
-	private int 			glWidth;
-	private int				glHeight;
-	private boolean			SurfaceCreated;
-
-
-	public GL(){
-		SurfaceCreated		= false;
-		glWidth				= 1;
-		glHeight			= 1;
 	}
- 
- 
+
 	@Override
- 	public void onSurfaceCreated(GL10 NOT_USED, EGLConfig config) {	
-		
-		SurfaceCreated		= true;
+	public void onSurfaceCreated(GL10 glUnused, EGLConfig config){
+		GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	}
- 
- 
-     @Override
-    public void onSurfaceChanged(GL10 NOT_USED, int width, int height) {
+
+
+	@Override
+	public void onSurfaceChanged(GL10 glUnused, int width, int height){
 		
-	}
- 
- 
-    @Override
-    public void onDrawFrame(GL10 NOT_USED) {
-		
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT| GLES20.GL_DEPTH_BUFFER_BIT);
-    }	
-    	
- 
-    public abstract void onCreate(int width, int height, boolean contextLost);
-	public abstract void onDrawFrame(boolean firstDraw);	
+	}	
+
+
+	@Override
+	public void onDrawFrame(GL10 glUnused){
+		GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);	
+ 	}	
 }
 
-
-/*
-public class GL implements Renderer {
-	private Cube   cube;
-	private Camera Cam; 
-
-
-	private float rquadx; 
-	private float rquady; 
-	private float rquadz; 
-
-	public GL(){
-		cube = new Cube();
-		Cam  = new Camera();
-	}
-
-
-	public void onSurfaceCreated(GL10 gl, EGLConfig config) {		
-		gl.glShadeModel(GL10.GL_SMOOTH); 			//Enable Smooth Shading
-		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f); 	//Black Background
-		gl.glClearDepthf(1.0f); 					//Depth Buffer Setup
-		gl.glEnable(GL10.GL_DEPTH_TEST); 			//Enables Depth Testing
-		gl.glDepthFunc(GL10.GL_LEQUAL); 			//The Type Of Depth Testing To Do
-		
-		//Really Nice Perspective Calculations
-		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST); 
-	}
-
-
-	public void onDrawFrame(GL10 gl) {
-		//Clear Screen And Depth Buffer
-		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);	
-		gl.glLoadIdentity();			//Reset The Current Modelview Matrix		
-		
-		// Set Camera here
-		Cam.SetCamera(gl);
-	   
-		//
-		// Draw Shit here!
-
-		//Drawing
-		gl.glPushMatrix();
-
-		gl.glTranslatef(0.0f, 0.2f, 0.0f);	//Move down 1.0 Unit And Into The Screen 7.0
-		//Minor change: Scale the Cube to 80 percent, otherwise it would be too large for the Emulator screen
-		gl.glScalef(0.9f, 0.9f, 0.9f); 			
-		gl.glRotatef(rquadx, 1.0f, 0.0f, 0.0f);	//Rotate The Square On The X axis 
-		gl.glRotatef(rquady, 0.0f, 1.0f, 0.0f);	//Rotate The Square On The X axis 
-		gl.glRotatef(rquadz, 0.0f, 0.0f, 1.0f);	//Rotate The Square On The X axis 
-		cube.draw(gl);
-		//Draw the Cube
-		gl.glPopMatrix();		
- 
-		rquadx -= 1.5f;			//Decrease The Rotation Variable For The Quad ( NEW )
-		rquady += 1.5f;			//Decrease The Rotation Variable For The Quad ( NEW )
-		rquadz += 2.5f;			//Decrease The Rotation Variable For The Quad ( NEW )
-	}
-
-
-	public void onSurfaceChanged(GL10 gl, int width, int height) {
-		if(height == 0) { 					//Prevent A Divide By Zero By
-			height = 1; 					//Making Height Equal One
-		}
-
-		gl.glViewport(0, 0, width, height); 	//Reset The Current Viewport
-		gl.glMatrixMode(GL10.GL_PROJECTION); 	//Select The Projection Matrix
-		gl.glLoadIdentity(); 					//Reset The Projection Matrix
-
-			//Calculate The Aspect Ratio Of The Window
-		GLU.gluPerspective(gl, 45.0f, (float)width / (float)height, 0.1f, 100.0f);
-
-		gl.glMatrixMode(GL10.GL_MODELVIEW); 	//Select The Modelview Matrix
-		gl.glLoadIdentity(); 			//Reset The Modelview Matrix
-	}
-}
-
-/*
-
-import android.opengl.GLES20;
- 
-public class GLES20Renderer extends GLRenderer {
- 
-    @Override
-    public void onCreate(int width, int height,
-            boolean contextLost) {
-        GLES20.glClearColor(0f, 0f, 0f, 1f);
-    }
- 
-    @Override
-    public void onDrawFrame(boolean firstDraw) {
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT
-                | GLES20.GL_DEPTH_BUFFER_BIT);
-    }
-} 
-
-
-*/
--- src/com/exam/game/run/GameRun.java
+++ src/com/exam/game/run/GameRun.java
@@ -17,33 +17,25 @@ public class GameRun extends Activity{
 	
 	String  LOG = "GameRun";
 	private GLSurfaceView glSurface;
-	/** Called when the activity is first created. */
-
-
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		glSurface = new GLSurfaceView(this);
 
-		final ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-		final ConfigurationInfo cInfo = am.getDeviceConfigurationInfo();
+		final ActivityManager am 			= (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+		final ConfigurationInfo cInfo		= am.getDeviceConfigurationInfo();
 		
 		if(cInfo.reqGlEsVersion >= 0x20000){
 			Log.i(LOG, "Supporting GLES 2.0:     <================================");
+			glSurface.setEGLContextClientVersion(2);
+			glSurface.setRenderer(new GL()); 
 		}else{
 			Log.i(LOG, "no Support for GLES 2.0: <================================");
 			Log.i(LOG, "Fall back to GLES 1.x:   <================================");
 		}
 		
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		//Create an Instance with this Activity
-		glSurface = new GLSurfaceView(this);
-		//Set our own Renderer
-		// dus de class uit de GLloop.java zit hier? hier in dit geval de naam van de dir
-		glSurface.setRenderer(new GL());    
-		//Set the GLSurface as View to this Activity
-		
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);	
 		setContentView(glSurface);
 	}
 
@@ -53,11 +45,10 @@ public class GameRun extends Activity{
 		glSurface.onResume();
 	}
 
-
 	@Override
 	protected void onPause() {
 		super.onPause();
 		glSurface.onPause();
-	}
- 
+	} 
 }
+
